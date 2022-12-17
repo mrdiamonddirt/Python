960f3537b8289bf1f31037477d2bc0d998e292fc
RowDog <mrdiamonddirt@gmail.com>
2022-12-17 00:32:26 +0000

    validate year and month

--- postcodecrime.py
+++ postcodecrime.py
@@ -5,6 +5,16 @@ postcode = input("Enter a postcode: ")
 year = input("Enter the year (YYYY): ")
 month = input("Enter the month (MM): ")
 
+# Validate the year input
+if not year.isdigit() or len(year) != 4:
+    print("Error: Invalid year")
+    exit()
+
+# Validate the month input
+if not month.isdigit() or int(month) < 1 or int(month) > 12:
+    print("Error: Invalid month")
+    exit()
+
 # Use the Postcodes.io API to retrieve the latitude and longitude of the location
 postcodes_url = f"https://api.postcodes.io/postcodes/{postcode}"
 response = requests.get(postcodes_url)
